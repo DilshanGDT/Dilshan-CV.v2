261eacd10202f93a6b192d9bba8579581e6753d2
Dilshan <tharakadilshan506@gmail.com>
2024-05-31 23:17:37 +0530

    Create header with hero image

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import Footer from "@/components/footer/Footer";
+import Header from "@/components/header/Header";
 import NavBar from "@/components/navbar/NavBar";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
       <NavBar />
+      <Header />
       <Footer />
     </main>
   );
