024e1555ea4b0b295076b3067e5e6fb39f967b09
Dilshan <tharakadilshan506@gmail.com>
2024-06-20 17:29:10 +0530

    Change Skills

--- components/servicess/Categories.tsx
+++ components/servicess/Categories.tsx
@@ -12,26 +12,26 @@ const Categories = () => {
           <div className="flex justifu-center items-center cursor-pointer">
             <GiCardboardBox className="w-10 h-10 bg-yellow-400 text-gray-100 m-3 p-1 shadow-xl rounded-md" />
             <h2 className="text-xl text-indigo-600 font-semibold">
-              Strategy & Direction
+              Web Developing
             </h2>
           </div>
-          <p className="md:text-base text-gray-400 md:text-center sm:text-left sm:text-sm">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. A, commodi
-            officia necessitatibus animi pariatur aut odit sed voluptatibus eos
-            dolores praesentium quas dolore, ab.
+          <p className="md:text-base text-gray-400 md:text-justify sm:text-left sm:text-sm">
+            As a Computer Science honors student passionate about web
+            development, learning cutting-edge technologies, and building
+            innovative and suitable web solutions.
           </p>
         </div>
         <div className="flex flex-col justify-center items-center">
           <div className="flex justifu-center items-center cursor-pointer">
             <HiColorSwatch className="w-10 h-10 bg-red-400 text-gray-100 m-3 p-1 shadow-xl rounded-md" />
             <h2 className="text-xl text-indigo-600 font-semibold">
-              Branding & Logo
+              Graphic Designing
             </h2>
           </div>
-          <p className="md:text-base text-gray-400 md:text-center sm:text-left sm:text-sm">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. A, commodi
-            officia necessitatibus animi pariatur aut odit sed voluptatibus eos
-            dolores praesentium quas dolore, ab.
+          <p className="md:text-base text-gray-400 md:text-justify sm:text-left sm:text-sm">
+            Passionate graphic designer skilled in creating visually stunning
+            and impactful designs. Dedicated to enhancing brand identity and
+            user experience.
           </p>
         </div>
       </div>
@@ -40,26 +40,26 @@ const Categories = () => {
           <div className="flex justifu-center items-center cursor-pointer">
             <AiOutlineBlock className="w-10 h-10 bg-green-400 text-gray-100 m-3 p-1 shadow-xl rounded-md" />
             <h2 className="text-xl text-indigo-600 font-semibold">
-              Motion & Animation
+              Project Management
             </h2>
           </div>
-          <p className="md:text-base text-gray-400 md:text-center sm:text-left sm:text-sm">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. A, commodi
-            officia necessitatibus animi pariatur aut odit sed voluptatibus eos
-            dolores praesentium quas dolore, ab.
+          <p className="md:text-base text-gray-400 md:text-justify sm:text-left sm:text-sm">
+            Experienced project manager adept at leading teams, managing
+            resources, and ensuring project success through effective planning
+            and execution. Specialized in AGILE.
           </p>
         </div>
         <div className="flex flex-col justify-center items-center">
           <div className="flex justifu-center items-center cursor-pointer">
             <MdDeveloperBoard className="w-10 h-10 bg-blue-400 text-gray-100 m-3 p-1 shadow-xl rounded-md" />
             <h2 className="text-xl text-indigo-600 font-semibold">
-              Design & Development
+              Team Collaboration
             </h2>
           </div>
-          <p className="md:text-base text-gray-400 md:text-center sm:text-left sm:text-sm">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. A, commodi
-            officia necessitatibus animi pariatur aut odit sed voluptatibus eos
-            dolores praesentium quas dolore, ab.
+          <p className="md:text-base text-gray-400 md:text-justify sm:text-left sm:text-sm">
+            Enthusiastic collaborator focused on fostering teamwork, enhancing
+            communication, and achieving collective goals through strong
+            interpersonal skills.
           </p>
         </div>
       </div>
